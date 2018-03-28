2e2f809f2310b520d9f24aae9d9fe0c0a6bb051a
neumanna94 <Guest@epicodus-103.local>
2018-03-28 10:42:45 -0700

    add table for listing keg items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,10 @@ import { Keg } from './models/keg.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  console.log("hi");
   title = 'Simple Microbrewery'; //Title property of app.component.html
-  
+  masterKegList: Keg[] = [
+    new Keg("One", "BrandOne", 50, .2, 10),
+    new Keg("Two", "BrandTwo", 100, .4, 20)
+  ];
 }
--- src/app/keg-list/keg-list.component.ts
+++ src/app/keg-list/keg-list.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Keg } from '../models/keg.model';
 @Component({
   selector: 'app-keg-list',
   templateUrl: './keg-list.component.html',
   styleUrls: ['./keg-list.component.css']
 })
-export class KegListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class KegListComponent {
+  @Input() childKegList: Keg[];
 }
