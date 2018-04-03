d936382fcf769bd3bf7a8cc8933f4a99e6c45511
Guest User <Guest@epicodus-103.local>
2018-04-03 08:54:33 -0700

    Update comments in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
+// Imported services from Angular.
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+// AppComponent directive that allows my manipulation of a particular html page with JavaScript.
 import { AppComponent } from './app.component';
+// Child components of AppComponent
 import { KegListComponent } from './keg-list/keg-list.component';
 import { NewKegComponent } from './new-keg/new-keg.component';
 import { EditKegComponent } from './edit-keg/edit-keg.component';
